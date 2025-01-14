73f837a3ccac7cf4c02dbd792dfaa2b2507fee8f
Eduardo Succo <eduardo.succo@gmail.com>
2025-01-14 17:18:13 +0000

    Modificando estrutura do layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,26 +55,26 @@ def gerar_pdf(nome_cliente, valor_final, parametros):
 
 # Função principal da aplicação
 def main():
-    st.title("Precificação de Projetos - WGG Digital Solutions")
+    st.title("Precificação de Projetos")
 
     # Entrada de dados do cliente
     nome_cliente = st.text_input("Nome do Cliente")
     estimativa_horas = st.number_input("Estimativa de Horas", min_value=1, value=300)
     desconto = st.slider("Aplicar Desconto (%)", min_value=0, max_value=15, value=0)
 
-    # Parâmetros do projeto
+    # Parâmetros do projeto com Checkbox ou Radio Button
     parametros = {
-        "escopo": st.selectbox("Escopo do Projeto", 
-                               ["Sem escopo definido", "Apenas com diretrizes gerais",
-                                "Informações suficientes para iniciar o trabalho",
-                                "Escopo detalhado e documentado"]),
-        "maturidade_cliente": st.selectbox("Maturidade do Cliente com Tecnologia e Dados", 
-                                           ["Baixa", "Média", "Alta"]),
-        "tecnologia": st.selectbox("Tecnologias para Desenvolvimento", 
-                                   ["Low Code", "Hard Code", "Misto"]),
-        "prazo_entrega": st.selectbox("Prazo de Entrega (Dias)", 
-                                      ["< 45 dias", "45 - 60 dias", "61 - 90 dias", "> 90 dias"]),
-        "complexidade": st.selectbox("Complexidade do Projeto", ["Baixa", "Média", "Alta"]),
+        "escopo": st.radio("Escopo do Projeto", 
+                           ["Sem escopo definido", "Apenas com diretrizes gerais",
+                            "Informações suficientes para iniciar o trabalho",
+                            "Escopo detalhado e documentado"]),
+        "maturidade_cliente": st.radio("Maturidade do Cliente com Tecnologia e Dados", 
+                                       ["Baixa", "Média", "Alta"]),
+        "tecnologia": st.radio("Tecnologias para Desenvolvimento", 
+                               ["Low Code", "Hard Code", "Misto"]),
+        "prazo_entrega": st.radio("Prazo de Entrega (Dias)", 
+                                  ["< 45 dias", "45 - 60 dias", "61 - 90 dias", "> 90 dias"]),
+        "complexidade": st.radio("Complexidade do Projeto", ["Baixa", "Média", "Alta"]),
     }
 
     # Calcular valor final
